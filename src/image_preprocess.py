47c8460b0e3870d76c019d34262ab54a606b9dbd
JohanLundberg12 <johanlundberg1993@gmail.com>
2023-06-29 01:07:48 +0200

    add resize_size as param

--- src/image_preprocess.py
+++ src/image_preprocess.py
@@ -1,20 +1,29 @@
+from typing import Tuple
+import torch
 import torchvision.transforms as transforms
 
 
 class ImagePreprocess:
-    def __init__(self, image_size=(512, 512)):
+    def __init__(
+        self,
+        image_size: Tuple[int, int] = (512, 512),
+        resize_size: Tuple[int, int] = (256, 256),
+    ) -> None:
         self.to_tensor = transforms.Compose(
             [
                 transforms.Resize(image_size),
                 transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)),
             ]
         )
-        self.resize = transforms.Resize((256, 256))
 
-    def process(self, image):
+        # 256, 256 in the case of pixel space editing
+        # 64, 64 in the case of latent space editing
+        self.resize = transforms.Resize(resize_size)
+
+    def process(self, image: torch.Tensor) -> torch.Tensor:
         return self.to_tensor(image).to("cuda")
 
-    def resize_mask(self, mask):
+    def resize_mask(self, mask: torch.Tensor) -> torch.Tensor:
         mask = self.resize(mask.unsqueeze(0))
         mask[mask < 1] = 0
         mask[mask > 1] = 1
