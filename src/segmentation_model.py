fba54d522eca5be6f782401451d1b3ce00ec72a2
JohanLundberg12 <johanlundberg1993@gmail.com>
2023-07-02 22:53:20 +0200

    segmentation moved to own class

---
+++ src/segmentation_model.py
@@ -0,0 +1,17 @@
+import torch
+
+from image_preprocess import ImagePreprocess
+from DDIMSegmentation.model import BiSeNet
+
+
+class SegmentationModel:
+    def __init__(self, net: BiSeNet, preprocess: ImagePreprocess) -> None:
+        self.net = net
+        self.preprocess = preprocess
+
+    def __call__(self, image: torch.Tensor) -> torch.Tensor:
+        t_image = self.preprocess.process(image)
+        out = self.net(t_image)[0]
+        segmentation = out.squeeze(0).argmax(0)
+
+        return segmentation
