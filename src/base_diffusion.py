672d3ffaffef340e32d16adf2f0e1a1e5e9bfa8a
JohanLundberg12 <johanlundberg1993@gmail.com>
2023-07-25 01:40:54 +0200

    base diffusion generate image method

--- src/base_diffusion.py
+++ src/base_diffusion.py
@@ -7,7 +7,22 @@ from diffusers import (
     UNet2DModel,
 )
 
-from utils import create_progress_bar, get_device, initialize_random_samples, set_seed
+from diffusion_utils import (
+    diffusion_loop,
+    single_step,
+    get_variance_noise,
+    get_noise_pred,
+)
+
+from transforms import tensor_to_pil, tensors_to_pils
+
+from utils import (
+    create_progress_bar,
+    get_device,
+    initialize_random_samples,
+    set_seed,
+    generate_random_samples,
+)
 
 
 class Diffusion:
@@ -27,16 +42,50 @@ class Diffusion:
     def decode(self, latent: torch.Tensor) -> torch.Tensor:
         raise NotImplementedError
 
+    def additional_prep(self, model, prompt):
+        return None
+
     def generate_image(
         self,
         xt,
-        eta,
-        zs,
-        num_inference_steps,
+        eta=0,
+        zs=None,
+        num_inference_steps=50,
         generator=None,
-        **kwargs,
+        prompt="",
+        cfg_scale=3.5,
     ):
-        raise NotImplementedError
+        self.model.scheduler.set_timesteps(num_inference_steps)
+
+        text_emb = self.additional_prep(self.model, prompt)
+
+        model_outputs = list()
+        pred_original_samples = list()
+
+        if eta > 0 and zs is None:
+            zs = generate_random_samples(
+                num_inference_steps, self.model.unet, generator=generator
+            )
+        for step_idx, timestep in diffusion_loop(model=self.model, zs=zs):
+            noise_pred = get_noise_pred(self.model, xt, timestep, text_emb, cfg_scale)
+            variance_noise = get_variance_noise(zs, step_idx, eta)
+
+            xt, pred_original_sample = single_step(
+                self.model, noise_pred, timestep, xt, eta, variance_noise
+            )
+
+            model_outputs.append(noise_pred)
+            pred_original_samples.append(pred_original_sample.detach())
+
+        x0 = xt
+
+        img = tensor_to_pil(x0)
+        pred_original_samples = torch.stack(
+            pred_original_samples, dim=0
+        ).squeeze()  # B, 1, C, H, W -> B, C, H, W
+        pred_original_samples = tensors_to_pils(pred_original_samples)
+
+        return img, model_outputs, pred_original_samples
 
     def generate_images(
         self,
