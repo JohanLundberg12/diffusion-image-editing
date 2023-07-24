2288364dda46b5b6de27e92ef479f43b2b262b0a
JohanLundberg12 <johanlundberg1993@gmail.com>
2023-07-25 01:39:23 +0200

    generate image moved to parent

--- src/diffusion_classes.py
+++ src/diffusion_classes.py
@@ -7,11 +7,9 @@ from diffusers import (
 
 from base_diffusion import Diffusion
 
-from transforms import tensors_to_pils, tensor_to_pil
-from utils import generate_random_samples
-from diffusion_utils import diffusion_loop, prep_text
-
 from transformers import CLIPTextModel, CLIPTokenizer
+
+from diffusion_utils import prep_text
 
 
 class SD(Diffusion):
@@ -37,47 +35,8 @@ class SD(Diffusion):
 
         return sample
 
-    def generate_image(
-        self,
-        xt,
-        eta=0,
-        zs=None,
-        num_inference_steps=50,
-        generator=None,
-        prompt="",
-        cfg_scale=3.5,
-    ):
-        self.model.scheduler.set_timesteps(num_inference_steps)
-
-        text_emb = prep_text(self.model, prompt)
-
-        if eta > 0 and zs is None:
-            zs = generate_random_samples(
-                num_inference_steps, self.model.unet, generator=generator
-            )
-        x0, model_outputs, pred_original_samples = diffusion_loop(
-            self.model,
-            xt,
-            eta,
-            zs,
-            text_emb=text_emb,
-            cfg_scale=cfg_scale,
-        )
-
-        sample = self.decode(x0)
-
-        pred_original_samples = torch.stack(
-            pred_original_samples, dim=0
-        ).squeeze()  # B, 1, C, H, W -> B, C, H, W
-
-        pred_original_samples = [
-            tensor_to_pil(  # to pil to save memory for ldm and sd models
-                self.decode(pred_original_sample.unsqueeze(0))  # C, H, W -> 1, C, H, W
-            )
-            for pred_original_sample in pred_original_samples
-        ]
-
-        return sample, model_outputs, pred_original_samples
+    def additional_prep(self, model, prompt):
+        return prep_text(model, prompt)
 
 
 class LDM(Diffusion):
@@ -102,37 +61,6 @@ class LDM(Diffusion):
 
         return sample
 
-    def generate_image(
-        self, xt, eta=0, zs=None, num_inference_steps=50, generator=None
-    ):
-        self.model.scheduler.set_timesteps(num_inference_steps)
-
-        if eta > 0 and zs is None:
-            zs = generate_random_samples(
-                num_inference_steps, self.model.unet, generator=generator
-            )
-        x0, model_outputs, pred_original_samples = diffusion_loop(
-            self.model,
-            xt,
-            eta,
-            zs,
-        )
-
-        sample = self.decode(x0)
-
-        pred_original_samples = torch.stack(
-            pred_original_samples, dim=0
-        ).squeeze()  # B, 1, C, H, W -> B, C, H, W
-
-        pred_original_samples = [
-            tensor_to_pil(  # to pil to save memory for ldm and sd models
-                self.decode(pred_original_sample.unsqueeze(0))  # C, H, W -> 1, C, H, W
-            )
-            for pred_original_sample in pred_original_samples
-        ]
-
-        return sample, model_outputs, pred_original_samples
-
 
 class DDPM(Diffusion):
     def __init__(self, model):
@@ -143,24 +71,3 @@ class DDPM(Diffusion):
 
     def decode(self, latent: torch.Tensor) -> torch.Tensor:
         return latent
-
-    def generate_image(
-        self, xt, eta=0, zs=None, num_inference_steps=50, generator=None, **kwargs
-    ):
-        self.model.scheduler.set_timesteps(num_inference_steps)
-
-        if eta > 0 and zs is None:
-            zs = generate_random_samples(
-                num_inference_steps, self.model.unet, generator=generator
-            )
-        x0, model_outputs, pred_original_samples = diffusion_loop(
-            self.model, xt, eta, zs
-        )
-
-        img = tensor_to_pil(x0)
-        pred_original_samples = torch.stack(
-            pred_original_samples, dim=0
-        ).squeeze()  # B, 1, C, H, W -> B, C, H, W
-        pred_original_samples = tensors_to_pils(pred_original_samples)
-
-        return img, model_outputs, pred_original_samples
